840868d66f0a5720b4efe71e7894f9c85f36121c
betiniakarandut <betiniakarandut@gmail.com>
2023-01-05 21:51:41 +0100

    Import 'a'

---
+++ 0x02-python-import_modules/5-variable_load.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# 5-variable_load.py
+
+if __name__ == "__main__":
+    """Print the value of variable a from variable_load_5."""
+    import a from variable_load_5
+    print(a)
