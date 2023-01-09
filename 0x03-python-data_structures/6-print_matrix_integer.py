7a22ec3640181e092d19bc2750f9effaeeefb969
betiniakarandut <betiniakarandut@gmail.com>
2023-01-09 04:04:18 +0100

    Print matrix

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# 6-print_matrix_integer.py
+
+
+def print_matrix_integer(matrix=[[]]):
+    """Print a matrix of integers."""
+    for a in range(len(matrix)):
+        for b in range(len(matrix[a])):
+                print("{:d}".format(matrix[a][b]), end="")
+                if b != (len(matrix[a]) - 1):
+                    print(" ", end="")
+
+        print("")
+
