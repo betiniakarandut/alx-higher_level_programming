972e6dbdd18010f0599f8cf6e3b66f3baf0ad809
betiniakarandut <betiniakarandut@gmail.com>
2023-01-13 00:32:14 +0100

    Fixed overindentation issues

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -19,14 +19,14 @@ def roman_to_int(roman_string):
     }
     num = 0
 
-    for j in range(len(roman_string)):
-        if roman_dict.get(roman_string[j], 0) == 0:
+    for i in range(len(roman_string)):
+        if roman_dict.get(roman_string[i], 0) == 0:
             return (0)
 
-        if (j != (len(roman_string) - 1) and
-                roman_dict[roman_string[j]] < roman_dict[roman_string[j + 1]]):
-                num += roman_dict[roman_string[j]] * -1
+        if (i != (len(roman_string) - 1) and
+                roman_dict[roman_string[i]] < roman_dict[roman_string[i + 1]]):
+                num += roman_dict[roman_string[i]] * -1
 
         else:
-            num += roman_dict[roman_string[j]]
+            num += roman_dict[roman_string[i]]
     return (num)
