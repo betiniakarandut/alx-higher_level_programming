9410cda40def793c4ca2aaad1bc391ee4af33097
betiniakarandut <betiniakarandut@gmail.com>
2023-01-12 05:34:55 +0100

    Returns the square of all elements in the dictionary

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# 9-multiple_by_2.py
+
+
+def multiply_by_2(a_dictionary):
+    """Return a new dictionary with all values multipled by 2."""
+    return ({i: a_dictionary[i] * 2 for i in a_dictionary})
