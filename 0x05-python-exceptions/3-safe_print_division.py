7b2488fd36b6dbc1db21db13724dd50e29a8a59e
betiniakarandut <betiniakarandut@gmail.com>
2023-01-23 22:28:13 +0100

    Division of two integers

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+
+def safe_print_division(a, b):
+    """Divides 2 integers and prints the result.
+    
+    Args:
+        a(int): First integer.
+        b(int): Second integer.
+
+    Returns:
+        The division of a by b.
+    """
+    try:
+        div = a / b
+    except (TypeError, ZeroDivisionError):
+        div = None
+    finally:
+        print("Inside result: {}".format(div))
+    return (div)
