d64856194d3a65dc7e3337257814ff008258d4f2
betiniakarandut <betiniakarandut@gmail.com>
2023-01-23 20:33:27 +0000

    Fixing Spacing and over-indentation

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -19,7 +19,9 @@ def safe_print_list(my_list=[], x=0):
         try:
             print("{}".format(my_list[i]), end"")
             res += 1
-        except IndexError:
+        except (IndexError, IndentationError):
+            print("Oops! Please ensure proper indexing
+                  and indentation")
         break
     print("")
     return (res)
