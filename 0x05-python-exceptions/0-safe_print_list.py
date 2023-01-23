ef6bb9a75adee218a86f8a22041c0b9a2ead2fab
betiniakarandut <betiniakarandut@gmail.com>
2023-01-23 20:09:41 +0000

    Prints x number of elements from a list

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+#0-safe_print_list.py
+#Author:Betini Akarandut
+
+def safe_print_list(my_list=[], x=0):
+    """Prints element of a list.
+
+    Args:
+        my_list(List): List to print out element from.
+        x(int): Number of items to print from the list.
+
+    Returns:
+        The number of items printed.
+
+    """
+    res = 0
+
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end"")
+            res += 1
+        except(IndexError, IndentationError):
+            print("Oops! Please check and ensure proper indexing and indentation")
+        break
+    print("")
+    return (res)
+
