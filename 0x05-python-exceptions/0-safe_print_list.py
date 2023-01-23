78887d8706c7f74b0751bf24c6b86128e5938ee4
betiniakarandut <betiniakarandut@gmail.com>
2023-01-23 21:00:24 +0000

    Added equal-to symbol

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -17,11 +17,11 @@ def safe_print_list(my_list=[], x=0):
 
     for i in range(x):
         try:
-            print("{}".format(my_list[i]), end"")
+            print("{}".format(my_list[i]), end="")
             res += 1
         except (IndexError, IndentationError):
             print("Oops! Please ensure proper indexing
                   and indentation")
-        break
+            break
     print("")
     return (res)
