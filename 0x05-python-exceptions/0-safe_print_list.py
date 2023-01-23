8599c3b73bcef70b4721663b547970b51ca7c3a4
betiniakarandut <betiniakarandut@gmail.com>
2023-01-23 20:24:29 +0000

    Fixing Spacing and over-indentation

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-#0-safe_print_list.py
-#Author:Betini Akarandut
+# 0-safe_print_list.py
+# Author:Betini Akarandut
 
 def safe_print_list(my_list=[], x=0):
     """Prints element of a list.
@@ -19,9 +19,9 @@ def safe_print_list(my_list=[], x=0):
         try:
             print("{}".format(my_list[i]), end"")
             res += 1
-        except(IndexError, IndentationError):
-            print("Oops! Please check and ensure proper indexing and indentation")
+        except (IndexError, IndentationError):
+            print("Oops! Please check and ensure proper indexing
+                  and indentation")
         break
     print("")
     return (res)
-
