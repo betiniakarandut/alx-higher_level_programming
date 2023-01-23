a209ec835a16087c52f5d05f3f71cb7f034ae3c9
betiniakarandut <betiniakarandut@gmail.com>
2023-01-23 22:27:27 +0100

    Fixing Indentation issues

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,6 +2,7 @@
 # 0-safe_print_list.py
 # Author:Betini Akarandut
 
+
 def safe_print_list(my_list=[], x=0):
     """Prints element of a list.
 
@@ -20,8 +21,7 @@ def safe_print_list(my_list=[], x=0):
             print("{}".format(my_list[i]), end="")
             res += 1
         except (IndexError, IndentationError):
-            print("Oops! Please ensure proper indexing
-                  and indentation")
+            print("Oops! Please give proper index")
             break
     print("")
     return (res)
