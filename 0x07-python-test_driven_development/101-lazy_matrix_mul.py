7d309a368cda44d416e00c69f1972802d2e3c292
betiniakarandut <betiniakarandut@gmail.com>
2023-01-31 10:14:52 +0100

    Uploading finished files

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -1,3 +1,16 @@
 #!/usr/bin/python3
-# file_name:
-# Betini Akarandut <betiniakarandut@gmail.com>
+# file_name:101-lazy_matrix_mul.py
+# Betini Akarandut
+"""Defines a matrix multiplication function using NumPy."""
+import numpy as np
+
+
+def lazy_matrix_mul(m_a, m_b):
+    """Return the multiplication of two matrices.
+
+    Args:
+        m_a (list of lists of ints/floats): The first matrix.
+        m_b (list of lists of ints/floats): The second matrix.
+    """
+
+    return (np.matmul(m_a, m_b))
