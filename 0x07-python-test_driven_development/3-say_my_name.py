adaf80d883ed1a20cc23d160010c9376eb5e605b
betiniakarandut <betiniakarandut@gmail.com>
2023-02-01 18:37:45 +0000

    Fixed spacing issues

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -12,6 +12,7 @@ def say_my_name(first_name, last_name=""):
     Raises:
         TypeError: If either of first_name or last_name are not strings.
     """
+
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
