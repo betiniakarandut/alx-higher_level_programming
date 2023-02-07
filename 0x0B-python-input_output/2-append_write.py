f676c60ca26b3402b968b74aca16a8607cc7ea96
betiniakarandut <betiniakarandut@gmail.com>
2023-02-08 00:47:45 +0100

    Optimized code

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -5,13 +5,14 @@
 
 
 def append_write(filename="", text=""):
-	"""Appends a string to the end of a UTF8 text file.
+    """Appends a string to the end of a UTF8 text file.
 
-	Args:
-		filename (str): The name of the file to append to.
-		text (str): The string to append to the file.
-	Returns:
-		The number of characters appended.
-	"""
-	with open(filename, "a", encoding="utf-8") as f:
-		return f.write(text)
+    Args:
+        filename (str): The name of the file to append to.
+        text (str): The string to append to the file.
+    Returns:
+        The number of characters appended.
+    """
+    with open(filename, "a", encoding="utf-8") as f:
+    	wr = f.write(text)
+        return wr
