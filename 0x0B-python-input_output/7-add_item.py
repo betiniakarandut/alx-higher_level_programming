27a1045b463148c3b96e64db7afc0c0bca0b2811
betiniakarandut <betiniakarandut@gmail.com>
2023-02-08 06:00:59 +0100

    Load, add, save

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -8,6 +8,6 @@ args = sys.argv
 filename = "add_item.json"
 with open(filename, 'a+', encoding="utf-8") as f:
     my_list = []
-    my_list.extend(args[1:])
+    my_list.append(args[1:])
     save_to_json_file(my_list, filename)
     load_from_json_file(filename)
