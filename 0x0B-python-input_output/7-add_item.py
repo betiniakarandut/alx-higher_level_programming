8f8e3ee44a20c303b6f4752383032648a2d58899
betiniakarandut <betiniakarandut@gmail.com>
2023-02-08 06:12:01 +0100

    Load, add, save

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python3
-# 7-add_item.py
-# Betini Akarandut
-"""Makes a list from args and saves them to a file in json format"""
+"""
+ save items to a file.
+"""
 
 import sys
-import json
+import os.path
 
-if __name__ == "__main__":
-    save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
-    load_from_json_file = \
-        __import__('8-load_from_json_file').load_from_json_file
+args = sys.argv[1:]
 
-    try:
-        items = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        items = []
-    items.extend(sys.argv[1:])
-    save_to_json_file(items, "add_item.json")
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+item = []
+if os.path.exists("./add_item.json"):
+    item = load_from_json_file("add_item.json")
+
+save_to_json_file(item + args, "add_item.json")
