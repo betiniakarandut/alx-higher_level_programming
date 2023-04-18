d83f240aaae931132891d03c7ad16010707a3800
betiniakarandut <betiniakarandut@gmail.com>
2023-04-18 03:19:10 +0000

    Filter states

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,17 +1,40 @@
 #!/usr/bin/python3
-# Displays all values in the states table of the database hbtn_0e_0_usa
-# whose name matches that supplied as argument.
-# Usage: ./2-my_filter_states.py <mysql username> \
-#                                <mysql password> \
-#                                <database name> \
-#                                <state name searched>
+"""
+Lists all states where name matches the
+argument from the database hbtn_0e_0_usa.
+"""
+
 import sys
 import MySQLdb
 
+
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    c.execute("SELECT * \
-                 FROM `states` \
-                WHERE BINARY `name` = '{}'".format(sys.argv[4]))
-    [print(state) for state in c.fetchall()]
+    # Get command line arguments
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    db_name = sys.argv[3]
+    state_name = sys.argv[4]
+
+    # Connect to MySQL database
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=mysql_username,
+                         passwd=mysql_password,
+                         db=db_name)
+    cur = db.cursor()
+
+    # Construct SQL query with format
+    query = "SELECT * FROM states WHERE name LIKE BINARY '{}'
+    ORDER BY id ASC".format(state_name)
+
+    # Execute SQL query
+    cur.execute(query)
+
+    # Fetch all results and print them
+    results = cur.fetchall()
+    for row in results:
+        print(row)
+
+    # Close database cursor and connection
+    cur.close()
+    db.close()
