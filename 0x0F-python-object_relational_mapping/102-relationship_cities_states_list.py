82160621cd20a653123e5e3ecd95a40c0a5b8d41
betiniakarandut <betiniakarandut@gmail.com>
2023-04-18 05:22:47 +0000

    From city relationship

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -1,20 +1,33 @@
 #!/usr/bin/python3
-# Lists all City objects from the database hbtn_0e_101_usa.
-# Usage: ./102-relationship_cities_states_list.py <mysql username> /
-#                                                 <mysql password> /
-#                                                 <database name>
+"""
+lists all City objects from the database hbtn_0e_101_usa
+"""
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from relationship_city import Base, City
 from relationship_state import State
-from relationship_city import City
 
-if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+if __name__ == '__main__':
+    # script should take 3 arguments: mysql username, mysql password and database name
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+
+    # create SQLAlchemy engine to connect to MySQL server
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(mysql_username, mysql_password, database_name),
                            pool_pre_ping=True)
+
+    # create a configured "Session" class
     Session = sessionmaker(bind=engine)
+
+    # create a Session
     session = Session()
 
-    for city in session.query(City).order_by(City.id):
-        print("{}: {} -> {}".format(city.id, city.name, city.state.name))
+    # retrieve all City objects and their linked State object
+    cities = session.query(City).order_by(City.id).all()
+
+    # print the desired output
+    for city in cities:
+        print('{}: ({}) {}'.format(city.state.name, city.id, city.name))
