a34f8b7070d933b6af16d0c9ac95ac990ac3666a
betiniakarandut <betiniakarandut@gmail.com>
2023-04-18 04:29:40 +0000

    First stateContains

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,21 +1,40 @@
 #!/usr/bin/python3
-# Lists all State objects that contain the letter a
-# from the database hbtn_0e_6_usa.
-# Usage: ./9-model_state_filter_a.py <mysql username> /
-#                                    <mysql password> /
-#                                    <database name>
+"""
+Script that lists all State objects that contain the letter a
+from the database hbtn_0e_6_usa.
+"""
+
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import State
+from model_state import Base, State
+
 
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+
+    # Initializes engine for connection to the database
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".
+                           format(mysql_username, mysql_password, database_name))
+
+    # Creates all tables stored in this metadata.
+    Base.metadata.create_all(engine)
+
+    # Creates a session factory bound to the engine
     Session = sessionmaker(bind=engine)
+
+    # Create a session
     session = Session()
 
-    for state in session.query(State).order_by(State.id):
-        if "a" in state.name:
-            print("{}: {}".format(state.id, state.name))
+    # Querying the database for all states containing letter a
+    contd = filter(State.name.like('%a%')).order_by(State.id).all()
+    states = session.query(State).contd
+
+    # Printing the results
+    for state in states:
+        print("{}: {}".format(state.id, state.name))
+
+    # Close the session
+    session.close()
