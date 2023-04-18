8c51e0310500fc2f1775b26aab047717cf9d7aaf
betiniakarandut <betiniakarandut@gmail.com>
2023-04-18 05:10:13 +0000

    City relationship2

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -1,23 +1,25 @@
 #!/usr/bin/python3
-# Defines a State model.
-# Inherits from SQLAlchemy Base and links to the MySQL table states.
+"""Defines the State class"""
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
-from sqlalchemy.ext.declarative import declarative_base
 from relationship_city import Base, City
 
 
 class State(Base):
     """Represents a state for a MySQL database.
 
-    Attributes:
-        __tablename__ (str): The name of the MySQL table to store States.
-        id (sqlalchemy.Integer): The state's id.
-        name (sqlalchemy.String): The state's name.
-        cities (sqlalchemy.orm.relationship): The State-City relationship.
-    """
-    __tablename__ = "states"
-    id = Column(Integer, primary_key=True)
-    name = Column(String(128), nullable=False)
+    Inherits from SQLAlchemy Base and links to the MySQL table states.
 
-    cities = relationship("City", backref="state", cascade="all, delete")
+    Attributes:
+        id (int): unique id generated automatically, cannot be null,
+            primary key
+        name (str): name of the state, cannot be null
+        cities (relationship): represents a relationship with the City class.
+            If a State object is deleted, all linked City objects will be
+            automatically deleted.
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, autoincrement=True, unique=True, nullable=False,
+                primary_key=True)
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", cascade="all, delete-orphan", backref="state")
