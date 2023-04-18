295b727dcf5eac562d9983b10375a358dc90fc2e
betiniakarandut <betiniakarandut@gmail.com>
2023-04-18 03:07:25 +0000

    Get all states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,20 +1,32 @@
 #!/usr/bin/python3
-# Displays all values in the states table of hbtn_0e_0_usa where name matches the argument.
-# Usage: ./2-my_filter_states.py <mysql username> \
-#                                <mysql password> \
-#                                <database name> \
-#                                <state name searched>
+# Lists all states from the database hbtn_0e_0_usa.
+# Usage: ./0-select_states.py <mysql username> \
+#                             <mysql password> \
+#                             <database name>
+
 import sys
 import MySQLdb
 
-def main():
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    query = "SELECT * FROM `states` WHERE `name` LIKE BINARY '{}' ORDER BY `id`".format(sys.argv[4])
-    c.execute(query)
-    [print(state) for state in c.fetchall()]
-    db.close()
-
 if __name__ == "__main__":
-    main()
+    # Connect to MySQL server
+    db = MySQLdb.connect(
+                         host="localhost",
+                         port=3306,
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[30]
+                        )
 
+    # Create a cursor object to execute queries
+    cursor = db.cursor()
+
+    # Execute SQL query to select all states sorted in ascending order by id
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+
+    # Fetch all rows and display them
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+
+    # Close database connection
+    db.close()
