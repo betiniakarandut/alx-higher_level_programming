9175f8d1e3a70dd25f39ba471c00464f99bf64a6
betiniakarandut <betiniakarandut@gmail.com>
2023-04-29 05:18:08 +0000

    Technical challenge: passed

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -6,13 +6,13 @@ import sys as s
 if __name__ == "__main__":
 
     url = "https://api.github.com./repos/{}/{}/commits".format(
-            s.argv[1], s.arigv[2])
+            s.argv[1], s.argv[2])
     res = requests.get(url)
-    details = res.json
+    details = res.json()
     for j in range(15):
         if res.status_code == 200:
-            print({}:{}.format(
+            print("{}:{}".format(
                   details[j].get("sha"),
-                  get.details[j]("commit").get("author").get("name")
+                  details[j].get("commit").get("author").get("name")))
         else:
-            print("Oops!".format(res.status_code))
+            print("Error code {}".format(details.status_code))
