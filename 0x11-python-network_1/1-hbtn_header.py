df3ec95036c807b6b9498693d1514cc2ca1e69c5
betiniakarandut <betiniakarandut@gmail.com>
2023-04-29 01:01:46 +0000

    Sends request and display value

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Script that takes in a URL, sends a request to the URL"""
+import urllib.request
+import sys as s
+
+url = s.argv[1]
+with urllib.request.urlopen(url) as res:
+    headers = res.info()
+    x_request_id = headers.get("X-Request-Id")
+    print(x_request_id)
