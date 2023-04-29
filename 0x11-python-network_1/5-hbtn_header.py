aeee82ec0fd2dfcb8b082c6d9dfb4e74d4b3b93b
betiniakarandut <betiniakarandut@gmail.com>
2023-04-29 02:59:53 +0000

    Response header value

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
-""""""
+"""Script that takes in a URL, sends a request to the URL"""
+import requests
+import sys as s
 
+if __name__ == "__main":
+    url = s.argv[1]
+    res = requests.get(url)
+    print(res.headers.get("X-Request-Id"))
