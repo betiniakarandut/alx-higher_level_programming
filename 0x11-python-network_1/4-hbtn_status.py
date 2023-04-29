46f3dc8bd20a36db1aa68a740565c6ae8445341d
betiniakarandut <betiniakarandut@gmail.com>
2023-04-29 02:44:49 +0000

    Added: if __name__ == __main__: construct

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """Script that fetches https://alx-intranet.hbtn.io/status"""
 import requests
+if __name__ == "__main__":
 
-url = 'https://alx-intranet.hbtn.io/status'
-res = requests.get(url)
+    url = 'https://alx-intranet.hbtn.io/status'
+    res = requests.get(url)
 
-print("Body response:")
-print("\t - type: {}".format(type(res.text)))
-print("\t - content: {}".format(res.text))
+    print("Body response:")
+    print("\t - type: {}".format(type(res.text)))
+    print("\t - content: {}".format(res.text))
