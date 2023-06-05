4fcd6e1a47f58f4f7cd2a4e7c471b909f0025d82
betiniakarandut <betiniakarandut@gmail.com>
2023-06-05 04:03:11 +0000

    Prints number of movies for given id

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const request = require('request');
+const apiUrl = process.argv[2];
+request(apiUrl, function (error, response, body) {
+  if (!error) {
+    const results = JSON.parse(body).results;
+    console.log(results.reduce((count, movie) => {
+      return movie.characters.find((character) => character.endsWith('/18/'))
+        ? count + 1
+        : count;
+    }, 0));
+  }
+});
